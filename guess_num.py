d2f21d4c537d10908574b54a35d1c9ee2588061e
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-10-13 17:55:57 +0530

    resolved all issues

--- guess_num.py
+++ guess_num.py
@@ -1,12 +1,16 @@
 import random
 
-number = random.randint(1, 10)
+target_number = random.randint(1, 10)
 guess = 0
-while guess != number:
-    guess = int(input("Guess a number between 1 and 10: "))
-    if guess < number:
+while guess != target_number:
+    try:
+        guess = int(input("Guess a number between 1 and 10: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
+    if guess < target_number:
         print("Too low!")
-    elif guess > number:
+    elif guess > target_number:
         print("Too high!")
     else:
         print("Correct!")
