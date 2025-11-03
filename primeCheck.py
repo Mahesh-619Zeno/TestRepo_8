39b81ed9e032450d329258904929f0f621b49084
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-11-03 17:30:06 +0530

    Fix variable name for prime number check

--- primeCheck.py
+++ primeCheck.py
@@ -3,17 +3,17 @@
 number_to_check = 29
 is_prime = True  # Assume it's prime until proven otherwise
 
-if number <= 1:
+if number_to_check <= 1:
     is_prime = False
 else:
     # Check factors up to the square root of the number
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
+    for i in range(2, int(number_to_check**0.5) + 1):
+        if number_to_check % i == 0:
             is_prime = False
             break
 
 # Output result
 if is_prime:
-    print(number, "is a prime number")
+    print(number_to_check, "is a prime number")
 else:
-    print(number, "is not a prime number")
+    print(number_to_check, "is not a prime number")
