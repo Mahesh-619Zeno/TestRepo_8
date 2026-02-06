8fb6a3e38a3ca1f4da18d9cd596af70c0d8ae888
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2026-02-06 16:54:55 +0530

    resolved another naming convention issue

--- sample_projects/Audio-to-Spectrogram/audio-to-spectrogram.py
+++ sample_projects/Audio-to-Spectrogram/audio-to-spectrogram.py
@@ -5,9 +5,9 @@ audio = 'path/to/your/audio/file'  #replace this with the path to your file
 
 x, sr = librosa.load(audio)
 X = librosa.stft(x)
-Xdb = librosa.amplitude_to_db(abs(X))
+spectrogram_db = librosa.amplitude_to_db(abs(X))
 plt.figure(figsize = (10, 5))
-librosa.display.specshow(Xdb, sr = sr, x_axis = 'time', y_axis = 'hz')
+librosa.display.specshow(spectrogram_db, sr = sr, x_axis = 'time', y_axis = 'hz')
 plt.colorbar()
 plt.title('Spectrogram of '+ audio)
 plt.show()  # This will show the plot
